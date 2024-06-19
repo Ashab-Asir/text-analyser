eed4b1b1d4c862ba60f0156e0db7cf6e722849c7
Ashab-Asir <ashabasir101@gmail.com>
2024-06-19 17:23:16 +0600

    word analyser project is create and do styling and split all the things in component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Container from "./Container"
+import Footer from "./Footer"
+import Header from "./Header"
+import "./index.css"
+function App() {
+  return (
+    <>
+    <Header></Header>
+    <Container></Container>
+    <Footer></Footer>
+    </>
+  )
+}
+
+export default App
